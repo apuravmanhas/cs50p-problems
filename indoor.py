dc1b7220bda346dd88d24f72e2c9dd1f86120664
WuXin <apuravmanhas123@gmail.com>
2026-03-22 17:44:27 +0000

    indoor voice - completed

---
+++ indoor.py
@@ -0,0 +1,8 @@
+#variable holding the user input
+low = input("Enter your word : ")
+
+#converting the user input to lowercase
+low = low.lower()
+
+#printing the user input in lowercase
+print(f"{low}")
