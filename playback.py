a465ae40d983c423431fe76d5aec622fd717cee7
WuXin <apuravmanhas123@gmail.com>
2026-03-23 18:14:51 +0000

    playback speed - completed

---
+++ playback.py
@@ -0,0 +1,8 @@
+# take input 
+pause = input("enter your word : ")
+
+# new variable to hold the user input 
+new = pause.replace(" ","...")
+
+#printing the user input with ... instead of spaces
+print(f"{new}")
